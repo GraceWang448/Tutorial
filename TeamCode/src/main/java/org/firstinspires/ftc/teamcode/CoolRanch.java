90235b32656a21e5e5afe9bb4e29cd85f10f1d92
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2021-12-11 03:26:26 -0600

    removed nextcore motion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoolRanch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CoolRanch.java
@@ -5,6 +5,8 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.nextcore.Mechanisms;
@@ -12,55 +14,72 @@ import org.firstinspires.ftc.teamcode.nextcore.Mechanisms;
 @Config
 @TeleOp(name = "Cool Ranch - 12/11/21")
 public class CoolRanch extends LinearOpMode {
-    // FIELD SPECIFIC DATA
-    public static double MAX_VALUE = 72;
 
-    public static double TILE = 24;
-    public static double HALF_TILE = TILE / 2;
-
-
-    enum Mode {
-        DRIVER_CONTROL,
-        AUTOMATIC_CONTROL
-    }
-
-    Mode currentMode = Mode.DRIVER_CONTROL;
+    private DcMotorEx leftFront, leftRear, rightRear, rightFront;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        Mechanisms mech = new Mechanisms(hardwareMap);
+//        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftRear = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightRear = hardwareMap.get(DcMotorEx.class, "rightBack");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+
+
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        Mechanisms mech = new Mechanisms(hardwareMap);
+        
+
+        telemetry.addLine("Ready to start");
+        telemetry.update();
         waitForStart();
         while (opModeIsActive() && !isStopRequested()) {
-            drive.update();
-            // Gamepad 1 - Michael
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            -gamepad1.left_stick_y,
-                            -gamepad1.left_stick_x,
-                            -gamepad1.right_stick_x
-                    )
-            );
+
+
+            double forward = -gamepad1.left_stick_y;
+            double turn = -gamepad1.left_stick_x;
+            double strafe = -gamepad1.right_stick_x;
+
+            leftFront.setPower(forward + turn + strafe);
+            rightFront.setPower(forward - turn - strafe);
+            leftRear.setPower(forward + turn - strafe);
+            rightRear.setPower(forward - turn + strafe);
 
 
             // Gamepad 2 - Atharv
             // Intake
-            if (gamepad2.left_trigger > 0) mech.runIntake(Mechanisms.intakeState.IN);
-            if (gamepad2.right_trigger > 0) mech.runIntake(Mechanisms.intakeState.OUT);
+            if (gamepad2.left_trigger > 0.5) {
+                mech.runIntake(Mechanisms.intakeState.IN);
+            } else {
+                mech.runIntake(Mechanisms.intakeState.OFF);
+            }
+            if (gamepad2.right_trigger > 0.5) {
+                mech.runIntake(Mechanisms.intakeState.OUT);
+            } else {
+                mech.runIntake(Mechanisms.intakeState.OFF);
+            }
 
             // Unlock Object
-            if(gamepad2.left_bumper) mech.unlockControl(Mechanisms.objectUnlockState.OPEN);
-            if(gamepad2.right_bumper) mech.unlockControl(Mechanisms.objectUnlockState.CLOSE);
+            if (gamepad2.left_bumper) mech.unlockControl(Mechanisms.objectUnlockState.OPEN);
+            if (gamepad2.right_bumper) mech.unlockControl(Mechanisms.objectUnlockState.CLOSE);
 
             // Slides
             if (gamepad2.dpad_up) mech.runSlides(Mechanisms.slideState.TOP);
-            if (gamepad2.dpad_left || gamepad2.dpad_right) mech.runSlides(Mechanisms.slideState.MIDDLE);
+            if (gamepad2.dpad_left || gamepad2.dpad_right)
+                mech.runSlides(Mechanisms.slideState.MIDDLE);
             if (gamepad2.dpad_down) mech.runSlides(Mechanisms.slideState.BOTTOM);
 
             // Ducks
-            if(gamepad2.a) mech.runDuckSpin(Mechanisms.duckSpinState.ON);
+            if (gamepad2.a) {
+                mech.runDuckSpin(Mechanisms.duckSpinState.ON);
+            } else {
+                mech.runDuckSpin(Mechanisms.duckSpinState.OFF);
+            }
         }
     }
 }
