be5b59beeb54e9bb551ec90223367904207d636c
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2022-02-08 15:10:53 -0600

    simple tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DuckTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.nextcore.Mechanisms;
+
+@Config
+@TeleOp(name = "Duck Test")
+public class DuckTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        Mechanisms mech = new Mechanisms(hardwareMap);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.left_bumper) {
+                mech.runDuckSpin(Mechanisms.duckSpinState.ON, Mechanisms.duckSpinSide.LEFT);
+            } else if (gamepad1.right_bumper) {
+                mech.runDuckSpin(Mechanisms.duckSpinState.ON, Mechanisms.duckSpinSide.RIGHT);
+            }
+        }
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/IntakeTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.nextcore.Mechanisms;
+
+@Config
+@TeleOp(name = "Intake Test")
+public class IntakeTest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        Mechanisms mech = new Mechanisms(hardwareMap);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                mech.runIntake(Mechanisms.intakeState.IN);
+            } else if (gamepad1.b) {
+                mech.runIntake(Mechanisms.intakeState.OUT);
+            }
+        }
+    }
+
+}
