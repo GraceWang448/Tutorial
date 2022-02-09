2db4b867caf37779af834aa1e269e0cf0004e125
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2022-02-09 15:31:03 -0600

    tuned drive constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,7 +55,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.014;
+    public static double kV = 0.016;
     public static double kA = 0.005;
     public static double kStatic = 0.001;
 
@@ -89,7 +89,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 73.47608671662739;
     public static double MAX_ACCEL = 73.47608671662739;
-    public static double MAX_ANG_VEL = Math.toRadians(360);
+    public static double MAX_ANG_VEL = Math.toRadians(7.0664795643493425);
     public static double MAX_ANG_ACCEL = Math.toRadians(360);
 
 
