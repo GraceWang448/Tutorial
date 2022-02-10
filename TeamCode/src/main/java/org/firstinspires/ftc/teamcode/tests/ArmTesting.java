933e636924746d7e82f1c214b781e66e4f6a33eb
KANCHI <ritesh.kanchi@hotmail.com>
2022-02-10 17:58:11 -0600

    weird commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ArmTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ArmTesting.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
 @TeleOp(name = "ArmTest")
@@ -12,14 +13,18 @@ public class ArmTesting extends LinearOpMode {
 
     private DcMotorEx arm;
 
+    private Servo box;
+
     public static double tickVelocity = 200;
 
     public static int positionOne = 0;
     public static int positionTwo = 38;
+    public static int positionThree = 275;
 
     @Override
     public void runOpMode() {
         arm = hardwareMap.get(DcMotorEx.class, "arm");
+        box = hardwareMap.get(Servo.class, "box");
 
         arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -32,10 +37,12 @@ public class ArmTesting extends LinearOpMode {
 
             if(gamepad1.a) {
                 runToPosition(positionOne,arm,tickVelocity);
+                box.setPosition(-0.4);
+                runToPosition(positionTwo,arm,tickVelocity);
             }
 
             if(gamepad1.b) {
-                runToPosition(positionTwo,arm,tickVelocity);
+                    runToPosition(positionTwo,arm,tickVelocity);
             }
         }
     }
